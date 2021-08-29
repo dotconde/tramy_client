8327fa5780ffe2d54d9c88479790caffea252ca5
dotconde <iamdeyvi@gmail.com>
2021-08-28 23:08:11 -0500

    Refactor DetailedProduct naming and svg imports

--- src/components/DetailedProduct/index.jsx
+++ src/components/DetailedProduct/index.jsx
@@ -1,36 +1,35 @@
-import React, { PureComponent } from 'react'
-import './DetailedProduct.css';
-import iconWhatsapp from '../../assets/icons/whatsapp_icon.svg'
-import iconCheck from '../../assets/icons/comprobar.svg'
+import React from "react";
+import "./styles.css";
+import { ReactComponent as WhatsappIcon } from "../../assets/icons/whatsapp.svg";
+import checkIcon from "../../assets/icons/check.svg";
 
-export class index extends PureComponent {
-  render() {
-    return (
-      <div className="detailed_product_container">
-        <section className="intro_container">
-          <h3>Hola, Deyvi 👋 </h3>
-          <p>¡Bienvenido a Tramy!</p>
-          <hr />
-        </section>
-        <section className="channels_container">
-          <h4>Canales Integrados</h4>
-          <article className="article_container">
-            <div className="social_network_icon">
-              <img src={iconWhatsapp } alt="whatsapp icon" />
-            </div>
-            <div className="social_network_description">
-              <h5>WhatsApp Business (Integración Oficial)</h5>
-              <p>Envía y recibe mensajes de WhatsApp a través de Tramy</p>
-            </div>
-            <div className="social_network_status">
-              <img src={iconCheck} alt="check icon" />
-              <p>Conectado</p>
-            </div>
-          </article>
-        </section>
-      </div>
-    )
-  }
+function DetailedProduct() {
+  return (
+    <div className="detailed_product_container">
+      <section className="intro_container">
+        <h3>Hola, Deyvi 👋 </h3>
+        <p>¡Bienvenido a Tramy!</p>
+        <hr />
+      </section>
+      <section className="channels_container">
+        <h4>Canales Integrados</h4>
+        <article className="article_container">
+          <div className="social_network_icon">
+            <WhatsappIcon />
+          </div>
+          <div className="social_network_description">
+            <h5>WhatsApp Business (Integración Oficial)</h5>
+            <p>Envía y recibe mensajes de WhatsApp a través de Tramy</p>
+          </div>
+          <div className="social_network_status">
+            {/* Wrap checkIcon within img for compatibility purposes */}
+            <img src={checkIcon} alt="check icon" />
+            <p>Conectado</p>
+          </div>
+        </article>
+      </section>
+    </div>
+  );
 }
 
-export default index
+export default DetailedProduct;
