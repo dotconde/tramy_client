106d50e83f2f2cbf727a9161a90f68afb57f2dca
dotconde <iamdeyvi@gmail.com>
2021-08-29 01:45:43 -0500

    Rename Home children components

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import "./home.css";
-import DetailedProduct from "../DetailedProduct";
-import OnboardingInfo from "../Onboarding";
+import "./styles.css";
+import Feed from "../Feed";
+import Onboarding from "../Onboarding";
 
 function Home() {
   return (
-    <div className="home_container">
-      <DetailedProduct />
-      <OnboardingInfo />
+    <div className="home">
+      <Feed />
+      <Onboarding />
     </div>
   );
 }
