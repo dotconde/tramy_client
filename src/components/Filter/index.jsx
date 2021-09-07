f2815a08be51a04436f69de2dcb76670a7fa0644
Yahaira Collado <yahairavittoria@gmail.com>
2021-09-07 18:54:30 -0500

    Add group select to filter

--- src/components/Filter/index.jsx
+++ src/components/Filter/index.jsx
@@ -4,7 +4,28 @@ import "./styles.css";
 function Filter() {
   return (
     <div className="filter">
-      Filtro: <a href="/">Todos</a>
+      <label for="filter">Filtro:</label>
+      <select name="browser">
+        <optgroup>
+          <option value="" selected>
+            Sin Filtrar
+          </option>
+        </optgroup>
+
+        <optgroup label="Asesor">
+          <option value="">Todos</option>
+          <option value="">Diego Torres</option>
+          <option value="">Pedro Suarez</option>
+          <option value="">Christian Meier</option>
+        </optgroup>
+
+        <optgroup label="Estado">
+          <option value="">Todos</option>
+          <option value="">Contactado</option>
+          <option value="">Cotizado</option>
+          <option value="">Pagado</option>
+        </optgroup>
+      </select>
     </div>
   );
 }
