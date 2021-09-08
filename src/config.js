6b60a44e5d9c63a81977eaf90e3a9a2f398f9980
dotconde <iamdeyvi@gmail.com>
2021-09-08 02:24:52 -0500

    Add config.js for Tramy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,22 @@
-export var BASE_URL = "http://localhost:3000";
+var BASE_URL;
 
-export const ENDPOINTS = {
+switch (process.env.NODE_ENV) {
+  case "production":
+    BASE_URL = "https://tramyback.herokuapp.com";
+    break;
+  case "test":
+    BASE_URL = "https://staging.tramyback.herokuapp.com";
+    break;
+  case "development":
+    BASE_URL = "http://localhost:3000";
+    break;
+  default:
+    BASE_URL = "http://localhost:3000";
+    break;
+}
+
+const ENDPOINTS = {
   LOGIN: "login",
 };
+
+export { BASE_URL, ENDPOINTS };
