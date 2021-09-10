23e6008f21f7751d6f57395c0d85a753135f1ba4
Yahaira Collado <yahairavittoria@gmail.com>
2021-09-10 16:24:45 -0500

    Update sidebar

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import "./styles.css";
+import Profile from "../Profile";
+import tramyLogo from "../../assets/logo/tramy_logo.svg";
+import { ReactComponent as HomeIcon } from "../../assets/icons/home.svg";
+import { ReactComponent as ChatIcon } from "../../assets/icons/chat.svg";
+import { ReactComponent as ClientIcon } from "../../assets/icons/client.svg";
+import { ReactComponent as FilterIcon } from "../../assets/icons/filter.svg";
+import { ReactComponent as TeamIcon } from "../../assets/icons/team.svg";
+import { ReactComponent as SetupIcon } from "../../assets/icons/setup.svg";
+import { ReactComponent as LogoutIcon } from "../../assets/icons/logout.svg";
+import { ReactComponent as BidirectionalArrowIcon } from "../../assets/icons/bidirectional-arrow.svg";
+import { NavLink } from "react-router-dom";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <section className="sidebar__logo">
+        <img src={tramyLogo} alt="Tramy logo" />
+      </section>
+      <hr />
+      <section className="sidebar__menu">
+        {/* Profile quick info */}
+        <Profile />
+
+        {/* Navigation options */}
+        <ul className="sidebar__list">
+          <li>
+            <NavLink exact activeClassName="active" to="/">
+              <HomeIcon />
+              Inicio
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="active" to="/chat">
+              <ChatIcon />
+              Chat
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="active" to="/chat" to="/clients">
+              <ClientIcon />
+              Clientes
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="active" to="/chat" to="/funnel">
+              <FilterIcon />
+              Embudo de Ventas
+            </NavLink>
+          </li>
+        </ul>
+      </section>
+      <hr />
+      <section className="sidebar__settings">
+        <ul className="sidebar__list">
+          {/* <li>
+            <NavLink to="/team">
+              <TeamIcon />
+              Equipo
+            </NavLink>
+          </li> */}
+          <li>
+            <NavLink activeClassName="active" to="/setup">
+              <SetupIcon />
+              Configuración
+            </NavLink>
+          </li>
+          <li>
+            <button className="logout">
+              <LogoutIcon /> Cerrar Sesión
+            </button>
+          </li>
+        </ul>
+      </section>
+      <section className="sidebar__collapse">
+        <BidirectionalArrowIcon />
+      </section>
+    </div>
+  );
+}
+
+export default Sidebar;
