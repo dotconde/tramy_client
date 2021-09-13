3419c67d55f49d0e53ace52be236625c57425016
Yahaira Collado <yahairavittoria@gmail.com>
2021-09-13 01:21:11 -0500

    Hide channel card in home view

--- src/components/Feed/index.jsx
+++ src/components/Feed/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./styles.css";
-import Channel from "../UI/Channel";
 import { ReactComponent as WhatsappIcon } from "../../assets/icons/whatsapp.svg";
 
 function Feed({ firstName = "Deyvi" }) {
@@ -14,7 +13,7 @@ function Feed({ firstName = "Deyvi" }) {
       </section>
 
       {/* Feed list of integrated channels */}
-      <section className="feed__channel">
+      {/* <section className="feed__channel">
         <h4>Canales Integrados</h4>
         <Channel
           icon={<WhatsappIcon />}
@@ -22,7 +21,7 @@ function Feed({ firstName = "Deyvi" }) {
           content={"Envía y recibe mensajes de WhatsApp a través de Tramy"}
           channelStatus={false}
         />
-      </section>
+      </section> */}
     </div>
   );
 }
