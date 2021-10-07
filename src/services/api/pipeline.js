f47b5a51c56e8e081b2573ca36a2a3f07817986a
dotconde <iamdeyvi@gmail.com>
2021-10-06 20:03:56 -0500

    Add api-service to get leads by pipeline

--- src/services/api/pipeline.js
+++ src/services/api/pipeline.js
@@ -3,3 +3,8 @@ import { ENDPOINTS } from "../../config";
 
 export const getPipelines = (config) =>
   api.get(`/${ENDPOINTS.PIPELINE}`, config).then((response) => response.data);
+
+export const getPipelineStageLeads = (id, config) =>
+  api
+    .get(`/${ENDPOINTS.PIPELINE}/${id}/leads_by_stage`, config)
+    .then((response) => response.data);
