aa4eea12c0a8d0b7a26909c20ecb27043fb79d0e
dotconde <iamdeyvi@gmail.com>
2021-10-08 11:33:46 -0500

    Add navlink in beta phase

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -55,7 +55,7 @@ function Sidebar() {
             <li>
               <NavLink activeClassName="active" to="/funnel">
                 <FilterIcon />
-                Embudo de Ventas
+                Embudo (Beta) 🔧
               </NavLink>
             </li>
           </ul>
