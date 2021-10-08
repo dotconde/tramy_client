8e8f05099de21c57384edaada6d1e1a1b23adc84
dotconde <iamdeyvi@gmail.com>
2021-10-08 12:57:37 -0500

    Load first pipeline by default

--- src/components/Pipeline/index.jsx
+++ src/components/Pipeline/index.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import "./styles.css";
 import Board from "react-trello";
+import Loader from "../UI/Loader";
 import useConfig from "../../hooks/useConfig";
-import { useQuery, useMutation } from "react-query";
-import { updateLead } from "../../services/api/lead";
+import { useQuery } from "react-query";
+import { getPipelines } from "../../services/api/pipeline";
 import { getPipelineStageLeads } from "../../services/api/pipeline";
 import { pipelineToReactTrelloData } from "../../helpers/formatters/react-trello";
 
@@ -11,42 +12,35 @@ function Pipeline() {
   // Config
   const { config } = useConfig();
 
-  // States
-  const [pipelineId, setPipelineId] = useState(1);
-
-  const [draggedLeadId, setDraggedLeadId] = useState(undefined);
-  // const [laneId, setLaneId] = useState(undefined);
-
-  const { data: currentPipeline } = useQuery(
-    ["pipeline", pipelineId],
-    async () => getPipelineStageLeads(pipelineId, config),
+  // Load list of pipelines
+  const { data: pipelines } = useQuery(
+    "pipelines",
+    async () => getPipelines(config),
     {
       retry: 3,
     }
   );
 
-  const { mutate } = useMutation(
-    async (updatedLead) => updateLead(draggedLeadId, updatedLead, config),
-    {}
-  );
+  const pipelineId = pipelines ? pipelines[0]?.id : undefined;
 
-  const onCardMoveAcrossLanes = (fromLaneId, toLaneId, cardId, index) => {
-    // console.log("fromLaneId", fromLaneId);
-    // console.log("toLaneId", toLaneId);
-    // console.log("cardId", cardId);
-    // console.log("index", index);
-    if (toLaneId && cardId) {
-      setDraggedLeadId(cardId);
-      mutate({ stage_id: toLaneId });
-    }
-  };
+  // Then get default (or selected) pipeline
+  const { data: currentPipeline, isLoading: isLoadingCurrentPipeline } =
+    useQuery(
+      ["pipeline", pipelineId],
+      async () => getPipelineStageLeads(pipelineId, config),
+      {
+        retry: 3,
+        enabled: Boolean(pipelineId),
+      }
+    );
 
-  return (
+  return isLoadingCurrentPipeline ? (
+    <Loader />
+  ) : (
     <Board
       hideCardDeleteIcon
       data={pipelineToReactTrelloData(currentPipeline)}
       style={{ backgroundColor: "#f5f5f5" }}
-      onCardMoveAcrossLanes={onCardMoveAcrossLanes}
     />
   );
 }
