d9a5631f1527d2985259d4a0bc4174d04babb250
dotconde <iamdeyvi@gmail.com>
2021-10-14 00:33:26 -0500

    Add query search for clients

--- src/components/Views/Clients/index.jsx
+++ src/components/Views/Clients/index.jsx
@@ -2,26 +2,29 @@ import React, { useState, useEffect } from "react";
 import { BASE_URL, ENDPOINTS } from "../../../config";
 import useToken from "../../../hooks/useToken";
 import { tableHeaders } from "../../../constants/client";
-// import Search from "../../UI/Search";
-// import Button from "../../UI/Button";
-// import ClientFilter from "../../ClientFilter";
+import Search from "../../UI/Search";
 import ClientTable from "../../ClientTable";
 import Loader from "../../UI/Loader";
 import { ReactComponent as PencilIcon } from "../../../assets/icons/pencil.svg";
 import { ReactComponent as TrashIcon } from "../../../assets/icons/trash.svg";
 import { ReactComponent as MessageIcon } from "../../../assets/icons/message.svg";
-// import { ReactComponent as AddUserIcon } from "../../../assets/icons/add-user.svg";
+import { useDebounce } from "use-debounce";
 import axios from "axios";
 import "./styles.css";
+// import Button from "../../UI/Button";
+// import ClientFilter from "../../ClientFilter";
+// import { ReactComponent as AddUserIcon } from "../../../assets/icons/add-user.svg";
 
 function Clients() {
   const [clients, setClients] = useState(null);
+  const [filter, setFilter] = useState("");
+  const [debouncedFilter] = useDebounce(filter, 1000);
 
-  const endpoint = `${BASE_URL}/${ENDPOINTS.LEAD}`;
   const { token } = useToken();
   const config = {
     headers: { Authorization: `Bearer ${token}` },
   };
+  const endpoint = `${BASE_URL}/${ENDPOINTS.LEAD}?query=${debouncedFilter}`;
 
   const fetchClients = () => {
     axios.get(endpoint, config).then((response) => setClients(response.data));
@@ -33,11 +36,17 @@ function Clients() {
     }
     fetchData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [debouncedFilter]);
 
   return clients ? (
     <div className="clients">
-      {/* <Search placeholder={"Buscar por nombre, asesor o estado"} /> */}
+      <Search
+        value={filter}
+        onChange={(event) => setFilter(event.target.value)}
+        placeholder={"Buscar cliente por nombre, teléfono o email"}
+        borderColor={"1px solid #dfdfdf"}
+      />
+
       <div className="clients__options">
         {/* <ClientFilter
           placeholder={"Sin filtrar"}
