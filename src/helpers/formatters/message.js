858af5511e08bfd308986154275681db222e8253
dotconde <iamdeyvi@gmail.com>
2021-11-01 19:29:08 -0500

    Include document in message formatter

--- src/helpers/formatters/message.js
+++ src/helpers/formatters/message.js
@@ -1,23 +1,32 @@
 import BlobImage from "../../components/UI/BlobImage";
+import BlobPdf from "../../components/UI/BlobPdf";
 
 export function truncate(text, length) {
   return text.length > length ? `${text.substring(0, length)}...` : text;
 }
 
-export function renderMessage(messsage) {
+export function renderMessage(message) {
   let renderedMessage;
-  switch (messsage.type) {
+  switch (message.type) {
     case "text":
-      renderedMessage = <p>{messsage?.text?.body}</p>;
+      renderedMessage = <p>{message?.text?.body}</p>;
       break;
     case "image":
-      renderedMessage = <BlobImage imageId={messsage?.image?.id} />;
+      renderedMessage = <BlobImage imageId={message?.image?.id} />;
+      break;
+    case "document":
+      renderedMessage = (
+        <BlobPdf
+          pdfId={message?.document?.id}
+          filename={message?.document?.filename}
+        />
+      );
       break;
     case "template":
       renderedMessage = (
         <p>
           <b>➡️ Plantilla: </b>
-          {messsage?.template?.body}
+          {message?.template?.body}
         </p>
       );
       break;
@@ -25,7 +34,7 @@ export function renderMessage(messsage) {
       renderedMessage = (
         <p>
           {`El siguiente formato será soportado próximamente: `}
-          <b>{`${messsage.type}`}</b>
+          <b>{`${message.type}`}</b>
         </p>
       );
       break;
